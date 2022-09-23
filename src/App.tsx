c6caad01b0795e02d7e2cd54a963656cb8064701
Nawed Ali <nawed2611@gmail.com>
2022-09-24 00:07:34 +0530

    intial commit with vite + react + typescript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-bold underline text-3xl">Hello MLH</h1>
+    </div>
+  )
+}
+
+export default App
